11a3d1309d3e1f44fe42a54e7ffac1fc2318bc57
Daniel <danthevip@gmail.com>
2024-09-04 18:26:20 +0300

    Restructure, ready for microservices, added Gateway

---
+++ server/Raven/Raven.Gateway/Program.cs
@@ -0,0 +1,13 @@
+using Raven.Gateway;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+Startup.Configure(app, app.Environment);
+
+app.Run();
---
+++ server/Raven/Raven.Gateway/Startup.cs
@@ -0,0 +1,23 @@
+﻿namespace Raven.Gateway;
+
+public class Startup(IConfiguration configuration)
+{
+    public IConfiguration Configuration { get; } = configuration;
+    
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public static void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+    }
+}
