0be2fb1d7f161a1a67cea5ccb1eab1d477a5fd8d
Daniel <danthevip@gmail.com>
2024-09-07 22:35:58 +0300

    Concern separation, slight cleanup

--- server/Raven/Raven.Auth/Startup.cs
+++ server/Raven/Raven.Auth/Startup.cs
@@ -1,27 +1,20 @@
-﻿using System.Reflection;
-using EvolveDb;
-using Microsoft.EntityFrameworkCore;
-using Npgsql;
+﻿using Microsoft.EntityFrameworkCore;
 using Raven.Auth.Application.Services;
 using Raven.Auth.Domain.Interfaces;
 using Raven.Auth.Domain.Services;
 using Raven.Auth.Infrastructure;
 using Raven.Auth.Infrastructure.Repositories;
-using static System.Console;
 
 namespace Raven.Auth;
 
 public class Startup(IConfiguration configuration)
 {
-    public IConfiguration Configuration { get; } = configuration;
-    
     public void ConfigureServices(IServiceCollection services)
     {
-        EnsureDatabaseCreated();
-        EvolveMigrate();
+        StartupUtils.EvolveMigrate(configuration);
         
         services.AddDbContext<AuthDbContext>(options =>
-            options.UseNpgsql(Configuration.GetConnectionString("AuthDatabaseConnection")));
+            options.UseNpgsql(configuration.GetConnectionString("AuthDatabaseConnection")));
 
         services.AddScoped<IAuthRepository, AuthRepository>();
         services.AddScoped<AuthService>();
@@ -42,43 +35,5 @@ public class Startup(IConfiguration configuration)
         }
 
         app.MapControllers();
-    }
-    
-    private static void EnsureDatabaseCreated()
-    {
-        const string databaseName = "auth";
-
-        using var connection = new NpgsqlConnection("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=postgres");
-        connection.Open();
-        using var command = new NpgsqlCommand($"SELECT 1 FROM pg_database WHERE datname = '{databaseName}'", connection);
-        var exists = command.ExecuteScalar() != null;
-        if (exists)
-        {
-            return;
-        }
-        using var createDbCommand = new NpgsqlCommand($"CREATE DATABASE {databaseName}", connection);
-        createDbCommand.ExecuteNonQuery();
-    }
-
-    private void EvolveMigrate()
-    {
-        var assembly = Assembly.Load("Raven.Auth.Infrastructure"); 
-        try
-        {
-            var cnx = new NpgsqlConnection(Configuration.GetConnectionString("AuthDatabaseConnection"));
-            var evolve = new Evolve(cnx, WriteLine)
-            {
-                EmbeddedResourceAssemblies = [assembly],
-                EmbeddedResourceFilters = new[] { "Raven.Auth.Infrastructure.Migrations" },
-                IsEraseDisabled = true,
-            };
-
-            evolve.Migrate();
-        }
-        catch (Exception ex)
-        {
-            WriteLine("Database migration failed.");
-            throw;
-        }
     }
 }
---
+++ server/Raven/Raven.Auth/StartupUtils.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+using EvolveDb;
+using Npgsql;
+
+namespace Raven.Auth;
+
+public static class StartupUtils
+{
+    
+    public static void EvolveMigrate(IConfiguration config)
+    {
+        EnsureDatabaseCreated();
+        
+        var assembly = Assembly.Load("Raven.Auth.Infrastructure"); 
+        try
+        {
+            var cnx = new NpgsqlConnection(config.GetConnectionString("AuthDatabaseConnection"));
+            var evolve = new Evolve(cnx, Console.WriteLine)
+            {
+                EmbeddedResourceAssemblies = [assembly],
+                EmbeddedResourceFilters = new[] { "Raven.Auth.Infrastructure.Migrations" },
+                IsEraseDisabled = true,
+            };
+
+            evolve.Migrate();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Database migration failed.");
+            throw;
+        }
+    }
+    
+    private static void EnsureDatabaseCreated()
+    {
+        const string databaseName = "auth";
+
+        using var connection = new NpgsqlConnection("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=postgres");
+        connection.Open();
+        using var command = new NpgsqlCommand($"SELECT 1 FROM pg_database WHERE datname = '{databaseName}'", connection);
+        var exists = command.ExecuteScalar() != null;
+        if (exists)
+        {
+            return;
+        }
+        using var createDbCommand = new NpgsqlCommand($"CREATE DATABASE {databaseName}", connection);
+        createDbCommand.ExecuteNonQuery();
+    }
+}
