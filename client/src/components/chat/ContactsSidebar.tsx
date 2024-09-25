a62dfac80e5eadc4740f5a02724a6e8cb4d771f5
ludmila.friptu <ludmila.friptu@iucosoft.com>
2024-09-25 13:53:35 +0300

    Add home page layout (pentru daniel to play)

--- client/src/components/chat/ContactsSidebar.tsx
+++ client/src/components/chat/ContactsSidebar.tsx
@@ -3,7 +3,7 @@ import { ScrollArea } from "@/components/ui/scroll-area"
 
 const ContactsSidebar = () => {
   return (
-    <ScrollArea className="bg-[#E5F3FA] w-full lg:w-1/3 p-4 rounded-2xl my-5 ml-5 h-[550px]"> {/* Set height here */}
+    <ScrollArea className="bg-[#E5F3FA] p-4 rounded-2xl h-[450px]"> {/* Set height here */}
       
       <ChatMessage 
        avatar="https://via.placeholder.com/61x60"
